69ab8e867a1d7202dfe511fc037f495db4754a52
Manpreet636 <75639402+Manpreet636@users.noreply.github.com>
2022-01-06 08:30:05 -0500

    updated

--- app/src/main/java/com/example/fitnessprojectandroid/My_Adapter.java
+++ app/src/main/java/com/example/fitnessprojectandroid/My_Adapter.java
@@ -12,19 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
-//public class My_Adapter {
-//}
-//import android.content.Context;
-//        import android.content.Intent;
-//        import android.view.LayoutInflater;
-//        import android.view.View;
-//        import android.view.ViewGroup;
-//        import android.widget.ImageView;
-//        import android.widget.TextView;
-//
-//        import androidx.annotation.NonNull;
-//        import androidx.constraintlayout.widget.ConstraintLayout;
-//        import androidx.recyclerview.widget.RecyclerView;
+
 
 public class My_Adapter extends RecyclerView.Adapter<My_Adapter.MyViewHolder> {
 
